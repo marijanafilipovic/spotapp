5dddf737ef43dc7c66361aeacb6d935209164cb5
Marijana <Marijana@remote1>
2019-05-08 14:00:01 +0200

    3 - Front page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -121,13 +121,10 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">Penjalista</a>
+                    <a href="#">Blokovi</a>
+                    <a href="#">Smerovi</a>
+                    <a href="https://github.com/marijanafilipovic/spotapp">GitHub</a>
                 </div>
                 <div id="map"></div>
             </div>
